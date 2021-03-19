5fe2bfa824edbafcd8528308a567b004aebf2edf
Nikolai Osipov <Ayakashi2.0@yandex.ru>
2021-03-19 19:11:38 +0300

    during

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <header :class="[$style.myheader]">My first Vue!</header>
+    <main>
+      <h3>Content</h3>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style module>
+.myheader {
+  color: yellowgreen;
+}
+</style>
